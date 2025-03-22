cb9da94bbe85f14d98db0b597b14d5e62b4442a0
sudonym-i <isickln08@gmail.com>
2025-03-22 09:53:00 -0600

    update

--- scraper.cpp
+++ scraper.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <curl/curl.h>
 #include <sstream>
 
-//points that detemine what char to start and end reads at
-//for example <a> , </a>
-const int IND_SIZE = 16;
-const char START[IND_SIZE] = "sy";
-const char END[IND_SIZE] = "<";
-
-bool equal_str(char *one, const char two[IND_SIZE]);
-
 size_t WriteCallback(void *contents, size_t size, size_t nmemb, std::string *output);
 
-std::string *breakpoints(std::string &html_content, const char start[IND_SIZE], const char end[IND_SIZE]);
-
 int main(int argc, char* argv[]) {
+
+    std::ofstream outfile("output.txt");
     
     // Initialize curl globally
     CURL *curl;
@@ -38,21 +31,21 @@ int main(int argc, char* argv[]) {
         // Perform the request
         res = curl_easy_perform(curl);
         // Check if the request was successful
-        std::string *parsed = breakpoints(*html_content, START, END);
         
         if(res != CURLE_OK) {
             std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
         } else {
             // Output the HTML content of the page
-            std::cout << *parsed << std::endl;
+            outfile << *html_content << std::endl;
         }
         // Clean up LOCAL
         curl_easy_cleanup(curl);
-        delete parsed;
+
     }
     // Clean up curl globally
     delete html_content;
     curl_global_cleanup();
+    outfile.close();
     return 0;
 }
 
@@ -63,32 +56,3 @@ size_t WriteCallback(void *contents, size_t size, size_t nmemb, std::string *out
     return total_size;
 }
 
-
-std::string *breakpoints(std::string &html_content, const char start[IND_SIZE], const char end[IND_SIZE]){
-    char *current = &html_content[0];
-    std::string *inside = new std::string;
-    while(*current != '\0'){
-
-        if( equal_str(current, start) ){
-            while( !(equal_str(current, end)) ){
-                (*inside).push_back(*current);
-                current++;
-            }
-            (*inside).push_back(',');
-        }
-    current++;
-    }
-    return inside;
-}
-
-bool equal_str(char *one, const char two[IND_SIZE]){
-    int i = 0;
-    while(*one != '\0' && two[i] != '\0'){
-        if(*one != two[i]){
-            return false;
-        }
-        i++;
-        one++;
-    }
-    return true;
-}
